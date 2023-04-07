078e33df3fd274260324599014282e76824d7419
Bilguun.Leaf-3 <bilguun.leaf-3@KEKW.local>
2023-04-07 11:43:48 +0800

    Send mail is Done

--- upload/handlers/lastImage.js
+++ upload/handlers/lastImage.js
@@ -1,30 +1,48 @@
 const AWS = require('aws-sdk');
 const lambda = new AWS.Lambda();
+const nodemailer = require("nodemailer");
 const client = new AWS.Rekognition();
+const { v4: uuidv4 } = require('uuid');
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const db = new DynamoDB();
 
 exports.handler = async(event) => {
+    const Name = event.Records[0].s3.object.key;
     const Bucket = "leaf3bbilguunbucket";
     const params = {
         Image: {
             S3Object: {
                 Bucket,
-                Name: "email.png"
+                Name
             },
         },
     }
     const response = await client.detectText(params).promise()
     const email = response.TextDetections.filter(el => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(el.DetectedText))
-    return await new Promise((resolve, reject) => {
-        const invokeParams = {
-            FunctionName: "upload-dev-send",
-            Payload: JSON.stringify({
-                email: email[0]
-            })
-        };
-
-        lambda.invoke(invokeParams, (err, result) => {
-            if(err) reject(err);
-            else resolve(result);
-        })
+    const json = { email: email[0].DetectedText, id: uuidv4() };
+    // const result = await db.putItem({
+    //     TableName: 'emails',
+    //     Item: marshall(json)
+    // });
+    // console.log(result);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "bilguun70b1123@gmail.com",
+            pass: 'vjxvpyivcxrsgbeh'
+        }
     })
+    const mailOptions = {
+        from: 'bilguun70b1123@gmail.com',
+        to: email[0].DetectedText,
+        subject: 'Sending Email using Node.js',
+        text: "Successfull"
+    }
+    const result = transporter.sendMail(mailOptions, (error, info) => {
+        if(error) return error;
+        else return info;
+    })
+    console.log(result);
 }
--- upload/handlers/send.js
+++ upload/handlers/send.js
@@ -1,25 +1,7 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-exports.handler = async(event) => {
-    const { DetectedText } = event;
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: DetectedText,
-            pass: 'vjxvpyivcxrsgbeh'
-        }
-    })
-
-    const mailOptions = {
-        from: 'bilguun70b1123@gmail.com',
-        to: DetectedText,
-        subject: 'Sending Email using Node.js',
-        text: "Successfull"
-    }
-
-    const response = transporter.sendMail(mailOptions, (error, info) => {
-        if(error) return error;
-        else return info;
-    })
-    return response;
-}
+// exports.handler = async(event) => {
+//     const { DetectedText } = event;
+//     console.log(event);
+//     return response;
+// }
